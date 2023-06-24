08f4e0e6574e80c99dd062d1f48bb2c0dab09467
Marco <bigdatajang@gmail.com>
2023-06-24 17:34:37 +0900

    feat: query resolver 추가

--- server.js
+++ server.js
@@ -1,5 +1,17 @@
 import { ApolloServer, gql } from "apollo-server";
 
+const tweets = [
+	{
+		id: "1",
+		text: "first one!",
+		author: { id: 1, username: "Marco" },
+	},
+	{
+		id: "2",
+		text: "second one",
+	},
+];
+
 const typeDefs = gql`
 	type User {
 		id: ID
@@ -14,9 +26,24 @@ const typeDefs = gql`
 		allTweets: [Tweet]
 		tweet(id: ID): Tweet
 	}
+	type Mutation {
+		postTweet(text: String, userId: ID): Tweet
+	}
 `;
 
-const server = new ApolloServer({ typeDefs });
+const resolvers = {
+	Query: {
+		allTweets() {
+			console.log("트윗을 가져옵니다");
+			return tweets;
+		},
+		tweet(root, { id }) {
+			return tweets.find(tweet => tweet.id === id);
+		},
+	},
+};
+
+const server = new ApolloServer({ typeDefs, resolvers });
 
 server.listen().then(({ url }) => {
 	console.log(`Running on ${url}`);
