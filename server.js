7f98e23cfa81a1197e512ac75b98770f209b2a7e
Marco <bigdatajang@gmail.com>
2023-06-24 18:55:01 +0900

    feat: rest api를 graphql로 마이그레이션(공개된 movie api 활용)

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 import { ApolloServer, gql } from "apollo-server";
+import fetch from "node-fetch";
 
 let tweets = [
 	{
@@ -42,10 +43,35 @@ const typeDefs = gql`
 		allUsers: [User!]!
 		allTweets: [Tweet]
 		tweet(id: ID): Tweet
+		allMovies: [Movie!]!
+		movie(id: ID): Movie
 	}
 	type Mutation {
 		postTweet(text: String, userId: ID): Tweet
 		deleteTweet(id: ID): Boolean
+	}
+	type Movie {
+		id: Int!
+		url: String!
+		imdb_code: String!
+		title: String!
+		title_english: String!
+		title_long: String!
+		slug: String!
+		year: Int!
+		rating: Float!
+		runtime: Float!
+		genres: [String]!
+		summary: String
+		description_full: String!
+		synopsis: String
+		yt_trailer_code: String!
+		language: String!
+		background_image: String!
+		background_image_original: String!
+		small_cover_image: String!
+		medium_cover_image: String!
+		large_cover_image: String!
 	}
 `;
 
@@ -61,6 +87,18 @@ const resolvers = {
 		allUsers() {
 			console.log("모든 사용자를 가져옵니다");
 			return users;
+		},
+		allMovies() {
+			return fetch("https://yts.mx/api/v2/list_movies.json")
+				.then(r => r.json())
+				.then(json => json.data.movies);
+		},
+		movie(_, { id }) {
+			return fetch(
+				`https://yts.mx/api/v2/movie_details.json?movie_id=${id}`
+			)
+				.then(r => r.json())
+				.then(json => json.data.movie);
 		},
 	},
 	Mutation: {
